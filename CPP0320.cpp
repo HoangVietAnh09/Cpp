291180d247327927e4146295eacf2c5449069a1f
Hoang Viet Anh <vietanh092004@gmail.com>
2023-11-22 10:38:28 +0700

    Cpp

--- CPP0320.cpp
+++ CPP0320.cpp
@@ -1,21 +1,41 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
+#define ll long long
+int a[10] = {0};
+int checkAlpha(string s){
+    if(s[0]-'0' == 0) return 0;
+    int length = s.length();
+    for(int i = 0; i < length; i++){
+        if(!isdigit(s[i])) return 0;
+    }
+    return 1;
+}
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
-    int t;
-    cin >> t;
+    int n;
+    cin >> n;
     cin.ignore();
-    while(t--){
+    while(n--){
         string s;
-        getline(cin, s);
-        map<char, bool> mp;
-        int tmp = 0;
-        if(s[1] == '0') tmp = 0;
-        for(int i = 0; i < s.length(); i++){
-            
+        cin >> s;
+        set<int> se;
+        int length = s.length();
+        for(int i = 0; i < length; i++){
+            se.insert(s[i]-'0');
+        }
+        int cnt = 1;
+        for(int i = 0; i <= 9; i++){
+            if(a[i] == 1) cnt++;
+        }
+        if(!checkAlpha(s)){
+            cout << "INVALID\n";
+        }else if(se.size() != 10){
+            cout << "NO\n";
+        }else{
+            cout << "YES\n";
         }
     }
-    return 0;
+
 }
